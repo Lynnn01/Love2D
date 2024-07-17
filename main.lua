02f49f7d18636f9810493374da0b4524642199eb
Lynnn01 <jetsadathegame@gmail.com>
2024-07-17 12:28:33 +0700

    optimize the code

--- main.lua
+++ main.lua
@@ -2,23 +2,27 @@ function love.load()
     anim8 = require "libraries/anim8"
     love.graphics.setDefaultFilter("nearest", "nearest")
 
-    player = {}
-    player.x = 400
-    player.y = 200
-    player.speed = 5
-    player.framerate = 0.1
-    player.sprite = love.graphics.newImage("sprites/parrot.png")
-    player.spriteSheet = love.graphics.newImage("sprites/player-sheet.png")
-    player.grid = anim8.newGrid(12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+    -- Player setup
+    player = {
+        x = 400,
+        y = 200,
+        speed = 5,
+        framerate = 0.1,
+        sprite = love.graphics.newImage("sprites/parrot.png"),
+        spriteSheet = love.graphics.newImage("sprites/player-sheet.png"),
+        animations = {}
+    }
 
-    player.animetions = {}
-    player.animetions.down = anim8.newAnimation(player.grid("1-4", 1), player.framerate)
-    player.animetions.left = anim8.newAnimation(player.grid("1-4", 2), player.framerate)
-    player.animetions.right = anim8.newAnimation(player.grid("1-4", 3), player.framerate)
-    player.animetions.up = anim8.newAnimation(player.grid("1-4", 4), player.framerate)
+    local grid = anim8.newGrid(12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
-    player.anim = player.animetions.left
+    player.animations.down = anim8.newAnimation(grid("1-4", 1), player.framerate)
+    player.animations.left = anim8.newAnimation(grid("1-4", 2), player.framerate)
+    player.animations.right = anim8.newAnimation(grid("1-4", 3), player.framerate)
+    player.animations.up = anim8.newAnimation(grid("1-4", 4), player.framerate)
 
+    player.anim = player.animations.left
+
+    -- Background setup
     background = love.graphics.newImage("sprites/background.png")
 end
 
@@ -27,26 +31,26 @@ function love.update(dt)
 
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed
-        player.anim = player.animetions.right
+        player.anim = player.animations.right
         isMoving = true
     end
     if love.keyboard.isDown("left") then
         player.x = player.x - player.speed
-        player.anim = player.animetions.left
+        player.anim = player.animations.left
         isMoving = true
     end
     if love.keyboard.isDown("up") then
         player.y = player.y - player.speed
-        player.anim = player.animetions.up
+        player.anim = player.animations.up
         isMoving = true
     end
     if love.keyboard.isDown("down") then
         player.y = player.y + player.speed
-        player.anim = player.animetions.down
+        player.anim = player.animations.down
         isMoving = true
     end
 
-    if isMoving == false then
+    if not isMoving then
         player.anim:gotoFrame(2)
     end
 
